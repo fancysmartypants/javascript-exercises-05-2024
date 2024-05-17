a4322bd6449a0c445e75750ac69cb08d1d39fd0a
fancysmartypants <128561269+fancysmartypants@users.noreply.github.com>
2024-05-17 08:20:20 -0700

    Pass test 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let str=string;
+    // if string is 'hello there'
+    let strArray=str.split("");
+    // this should output an array of e.g. [h,e,l,l,o, ,t,h,e,r,e]
+    let reverseArray = [];
+    for (let i=str.length-1;i>=0;i--){
+        reverseArray.push(strArray[i]);
+    }
+    let reversedString = reverseArray.join("");
+    return reversedString;
 };
 
 // Do not edit below this line
