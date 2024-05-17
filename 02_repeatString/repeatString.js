a8a245de17a1fdf41880ff198dac2f98ef6b7f21
fancysmartypants <128561269+fancysmartypants@users.noreply.github.com>
2024-05-17 07:52:21 -0700

    finish test 02_repeatString. Use for loop to repeat string.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    let num = number;
+    let str = string;
+    let repeatedString='';
+    if (num <0) {
+        return repeatedString="ERROR";
+    }
 
+    for (let i=0; i< num; i++){
+        repeatedString+=str;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
