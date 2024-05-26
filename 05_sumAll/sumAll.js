47bd37a2ab05a9545fadcf18a9dcd07186591566
fancysmartypants <128561269+fancysmartypants@users.noreply.github.com>
2024-05-26 12:48:12 -0700

    complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let ans=0;
+    if ((a<0)||(b<0)||(typeof(a)!="number")||(typeof(b)!="number")){
+        return `ERROR`;
+    }else if (a>b){
+        let num1=b;
+        let num2=a;
+        for ( let i=num1; i<=num2; i++){
+            ans+=i;
+        }
+        return ans;
+    }else{
+        for (let i=a; i<=b; i++){
+            ans+=i;
+        }
+        return ans;
+    }
 
 };
 
